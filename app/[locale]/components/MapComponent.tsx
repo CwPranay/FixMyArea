902adbf48cd08ac3cdfa583d51a82599a9e71846
Pranay Gurav <guravpranay81@gmail.com>
2025-08-05 22:15:49 +0530

    .

--- app/[locale]/components/MapComponent.tsx
+++ app/[locale]/components/MapComponent.tsx
@@ -3,9 +3,9 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
-import { useEffect } from 'react';
+import { LatLngExpression } from 'leaflet';
 
-// Fix Leaflet icon paths (you can skip this if using CDN icons)
+// Fix Leaflet icon path
 delete (L.Icon.Default.prototype as any)._getIconUrl;
 L.Icon.Default.mergeOptions({
   iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-red.png',
@@ -13,51 +13,53 @@ L.Icon.Default.mergeOptions({
   shadowUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png',
 });
 
-// Red icon definition (optional if CDN URL used)
 const redIcon = new L.Icon({
-  iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-red.png',
+  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png',
+  iconRetinaUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png',
+  shadowUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png',
   iconSize: [25, 41],
   iconAnchor: [12, 41],
   popupAnchor: [1, -34],
-  shadowUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png',
   shadowSize: [41, 41],
 });
 
-const issues: {
-  id: number;
-  position: [number, number]; // <- strict 2-item tuple
-  description: string;
-}[] = [
+const issues = [
   {
     id: 1,
-    position: [19.0760, 72.8777],
+    position: [19.0760, 72.8777] as LatLngExpression,
     description: 'Pothole near Andheri station',
   },
   {
     id: 2,
-    position: [19.2183, 72.9781],
+    position: [19.2183, 72.9781] as LatLngExpression,
     description: 'Broken street light in Borivali',
   },
 ];
 
-
-const centerPosition: [number, number] = [19.1071, 72.8376];
-
+const centerPosition: LatLngExpression = [19.1071, 72.8376];
 
 const MapComponent = () => {
   return (
-    <MapContainer center={centerPosition} zoom={12} scrollWheelZoom={true} className="h-[400px] w-full rounded-2xl z-0 shadow">
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
+    <div className="w-full max-w-[600px] h-[400px] rounded-2xl overflow-hidden shadow-lg border border-gray-200">
+      <MapContainer
+        center={centerPosition}
+        zoom={13}
+        scrollWheelZoom={true}
+        zoomControl={false}
+        className="w-full h-full"
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+          url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+        />
 
-      {issues.map(issue => (
-        <Marker key={issue.id} position={issue.position} icon={redIcon}>
-          <Popup>{issue.description}</Popup>
-        </Marker>
-      ))}
-    </MapContainer>
+        {issues.map((issue) => (
+          <Marker key={issue.id} position={issue.position} icon={redIcon}>
+            <Popup>{issue.description}</Popup>
+          </Marker>
+        ))}
+      </MapContainer>
+    </div>
   );
 };
 
