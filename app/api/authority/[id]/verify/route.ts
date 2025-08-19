e625a71fcce2e1b37847bdf6272268efd393bb35
Pranay Gurav <guravpranay81@gmail.com>
2025-08-19 19:10:03 +0530

    .

--- app/api/authority/[id]/verify/route.ts
+++ app/api/authority/[id]/verify/route.ts
@@ -1,28 +1,26 @@
-import { NextResponse,NextRequest } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
 import user from "@/models/user";
 import { connectDB } from "@/lib/db";
- 
-interface RouteParams {
-  params: {
-    id: string;
-  };
-}
 
-export async function PATCH(req : NextRequest, { params }: RouteParams){
 
-    try{
+export async function PATCH(
+    req: NextRequest,
+    { params }: { params: Promise<{ id: string }> }
+) {
+
+    try {
         await connectDB();
-        const { id } = params;
-        const {status} = await req.json();
-        const updated =await user.findByIdAndUpdate(id,{authorityVerified:status},{new:true});
-        if(!updated){
-            return NextResponse.json({message:"Authority not found"}, {status:404});
+        const { id } =await params;
+        const { status } = await req.json();
+        const updated = await user.findByIdAndUpdate(id, { authorityVerified: status }, { new: true });
+        if (!updated) {
+            return NextResponse.json({ message: "Authority not found" }, { status: 404 });
         }
-        return NextResponse.json({message:"Authority status updated successfully", authority: updated}, {status:200});
+        return NextResponse.json({ message: "Authority status updated successfully", authority: updated }, { status: 200 });
     }
-    catch(error){
+    catch (error) {
         console.error("Error updating authority status:", error);
-        return NextResponse.json({message:"Internal Server Error"}, {status:500});
+        return NextResponse.json({ message: "Internal Server Error" }, { status: 500 });
     }
 
 
