6c64be6f68c6357a822b6b2cf8e06566831d1eda
Pranay Gurav <guravpranay81@gmail.com>
2025-08-24 22:14:39 +0530

    .

--- app/[locale]/components/Herosection.tsx
+++ app/[locale]/components/Herosection.tsx
@@ -16,19 +16,19 @@ export default function HeroSection() {
   }
   else
        return (
-    <section className="pt-52 md:pt-20 lg:pt-20   min-h-[calc(100vh-80px)] [font-family:var(--font-poppins)] bg-gray-50 px-4">
+    <section className="pt-52  lg:pt-20    sm:h-auto md:height-auto  lg:min-h-[calc(100vh-80px)] [font-family:var(--font-poppins)] bg-gray-50 px-4">
       
        <div className="max-w-7xl  mx-auto grid grid-cols-1 md:grid-cols-2 items-center gap-10">
         
         {/* LEFT: Text & Buttons */}
-        <div className="flex flex-col items-center md:items-start text-center md:text-left justify-center">
+        <div className="flex  flex-col items-center md:items-start text-center md:text-left justify-center">
           <h1 className="text-4xl md:text-5xl font-bold text-gray-900 mb-4">
             {t('title')}
           </h1>
           <p className="text-lg md:text-xl text-gray-600 max-w-xl mb-6">
             {t('subTitle')}
           </p>
-          <div className="flex flex-col sm:flex-row gap-4">
+          <div className="flex  sm:flex-row gap-4">
             <Link
               href="/"
               className="btn-primary-gradient text-white px-6 py-2 rounded-md transition hover:opacity-90 shadow text-center"
